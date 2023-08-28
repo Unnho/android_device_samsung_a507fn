b51c64abc418c159ec3b025b1c0964ca93e4d339
Mossie Parker <sajidhossainchowdhury@gmail.com>
2023-08-28 23:48:27 +0530

    Adapt trees

---
+++ lineage_a507fn.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+## Inherit from generic products, most specific first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+## Inherit from a505fn device
+$(call inherit-product, device/samsung/a507fn/device.mk)
+
+## Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+## Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+## Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+## Device identifier, this must come after all inclusions
+PRODUCT_DEVICE := a507fn
+PRODUCT_NAME := lineage_a507fn
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A507FN
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_SHIPPING_API_LEVEL := 28
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+## Use the latest CTS approved build identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=a50s \
+    PRODUCT_NAME=a50sxx \
+    PRIVATE_BUILD_DESC="a50sxx-user 11 RP1A.200720.012 A507FNXXS9CUJ1 release-keys"
+
+BUILD_FINGERPRINT := samsung/a50sxx/a50s:11/RP1A.200720.012/A507FNXXS9CUJ1:user/release-keys
